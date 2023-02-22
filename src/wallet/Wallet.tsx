28c7e00230094137c73b594a7200b2b1b8e8c243
bmeeder22 <bmeeder22@gmail.com>
2023-02-22 10:11:59 -0600

    web3auth

--- src/wallet/Wallet.tsx
+++ src/wallet/Wallet.tsx
@@ -1,49 +1,160 @@
-import React, {ReactNode, useMemo} from 'react';
-import {ConnectionProvider, WalletProvider} from '@solana/wallet-adapter-react';
-import {
-  BackpackWalletAdapter,
-  BraveWalletAdapter,
-  Coin98WalletAdapter,
-  CoinbaseWalletAdapter,
-  FractalWalletAdapter,
-  GlowWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-  TrustWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import {WalletModalProvider} from '@solana/wallet-adapter-react-ui';
+import React, {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import {Connection, PublicKey, Transaction} from '@solana/web3.js';
+import RPC from '../solanaRpc';
+import {Web3Auth} from '@web3auth/modal';
+import {CHAIN_NAMESPACES, SafeEventEmitterProvider} from '@web3auth/base';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
+export interface WalletContextProps {
+  wallet: null;
+  connected: boolean;
+  publicKey: PublicKey | null;
+  connection: Connection | null;
+  sendTransaction: (transaction: Transaction) => Promise<string>;
+  signTransaction: (transaction: Transaction) => Promise<Transaction>;
+  signMessage: (message: string) => Promise<Uint8Array>;
+  connect: () => Promise<void>;
+  disconnect: () => Promise<void>;
+}
 
-export function Wallet({children}: {children: ReactNode}) {
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new SlopeWalletAdapter(),
-      new GlowWalletAdapter(),
-      new TorusWalletAdapter(),
-      new BackpackWalletAdapter(),
-      new Coin98WalletAdapter(),
-      new CoinbaseWalletAdapter(),
-      new BraveWalletAdapter(),
-      new FractalWalletAdapter(),
-      new TrustWalletAdapter(),
-    ],
-    []
+const WalletContext = createContext<WalletContextProps>({
+  wallet: null,
+  connected: false,
+  publicKey: null,
+  connection: null,
+  sendTransaction: () => Promise.reject(new Error('')),
+  signTransaction: () => Promise.reject(new Error('')),
+  signMessage: () => Promise.reject(new Error('')),
+  connect: () => Promise.reject(new Error('')),
+  disconnect: () => Promise.reject(new Error('')),
+});
+
+export function WalletContextProvider({children}: {children: ReactNode}) {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
   );
+
+  const [publicKey, setPublicKey] = useState<PublicKey | null>(null);
+
+  const connected = useMemo(() => !!publicKey, [publicKey]);
+
+  const connection = useMemo(() => {
+    const rpcUrl = web3auth?.options.chainConfig.rpcTarget;
+    if (!rpcUrl) return null;
+    return new Connection(rpcUrl, 'confirmed');
+  }, [web3auth?.options.chainConfig.rpcTarget]);
+
+  const sendTransaction = useCallback(async (transaction: Transaction) => {
+    if (!provider) {
+      throw new Error('provider not initialized yet');
+    }
+    const rpc = new RPC(provider);
+    return await rpc.sendTransaction(transaction);
+  }, []);
+
+  const signTransaction = useCallback(async (transaction: Transaction) => {
+    if (!provider) {
+      throw new Error('provider not initialized yet');
+    }
+    const rpc = new RPC(provider);
+    return await rpc.signTransaction(transaction);
+  }, []);
+
+  const signMessage = useCallback(async (message: string) => {
+    if (!provider) {
+      throw new Error('provider not initialized yet');
+    }
+    const rpc = new RPC(provider);
+    return await rpc.signMessage(message);
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId:
+            'BKPNPUxXPoDfsjTw6AF1dfMJdyzYeXMskYWmObAcicEdSYS91NVb_cMeIVZIi_M55jF-i3wiNRaVblr5347Clss',
+          web3AuthNetwork: 'testnet', // mainnet, aqua, celeste, cyan or testnet
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.SOLANA,
+            chainId: '0x3', // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+            rpcTarget:
+              'https://rpc-devnet.helius.xyz/?api-key=2f915565-3608-4451-9150-4e72f50f10c2', // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+        });
+
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const connect = async () => {
+    if (!web3auth) {
+      throw new Error('web3auth not initialized yet');
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      throw new Error('provider not initialized yet');
+    }
+    const rpc = new RPC(provider);
+    return await rpc.getAccounts();
+  };
+
+  useEffect(() => {
+    if (!provider) return;
+
+    getAccounts().then(([account]) => {
+      setPublicKey(new PublicKey(account));
+    });
+  }, [provider]);
+
+  const disconnect = async () => {
+    if (!web3auth) {
+      throw new Error('web3auth not initialized yet');
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
 
   return (
-    <ConnectionProvider
-      endpoint={
-        'https://phantasi-develope-8dd8.devnet.rpcpool.com/c4188c94-d47c-43d0-bbc0-bad3dbd2af4e'
-      }
+    <WalletContext.Provider
+      value={{
+        wallet: null,
+        connected,
+        publicKey,
+        sendTransaction,
+        signMessage,
+        connect,
+        disconnect,
+        connection,
+        signTransaction,
+      }}
     >
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+      {children}
+    </WalletContext.Provider>
   );
 }
+
+export const useWallet = () => useContext(WalletContext);
